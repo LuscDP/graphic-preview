e97dcbc34cec936e6e93cfe9d9127af1c104316c
Luis Saca <sacaluisv@gmail.com>
2023-12-07 11:10:47 -0400

    Update base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://luissaca.github.io/graphic-preview"
+  base: "https://luissaca.github.io/fisica"
 })
